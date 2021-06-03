6e88186deff53bef02facdceeb76d768124b16e2
jmarcos9 <66381477+jmarcos9@users.noreply.github.com>
2021-06-03 16:01:30 -0300

    add class ListandoReceitas para identificar no portal a receita pelo nome e deixando links para ediar

--- receitas/admin.py
+++ receitas/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from . models import Receita
 
+class ListandoReceitas(admin.ModelAdmin):
+    list_display = ('id', 'nome_receita', 'categoria','tempo_preparo','data_receita')
+    list_display_links = ('id','nome_receita')
 
-admin.site.register(Receita)
+
+admin.site.register(Receita, ListandoReceitas)
--- receitas/models.py
+++ receitas/models.py
@@ -9,3 +9,5 @@ class Receita(models.Model):
     rendimento = models.CharField(max_length=100)
     categoria = models.CharField(max_length=100)
     data_receita = models.DateField(default=datetime.now, blank=True)
+
+

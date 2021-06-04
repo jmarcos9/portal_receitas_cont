87d754a8eeac906fa0193fdd44a71c0329135b2f
jmarcos9 <66381477+jmarcos9@users.noreply.github.com>
2021-06-04 09:25:10 -0300

    add nova rota buscar e add def buscar chamando bscar.html. No index configurando action do form url 'buscar

--- receitas/urls.py
+++ receitas/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:receita_id>receita', views.receita, name='receita')
+    path('<int:receita_id>receita', views.receita, name='receita'),
+    path('buscar', views.buscar, name='buscar')
 ]

e6f9f59c8fd3b6da8245ff7d103694be0393abab
jmarcos9 <66381477+jmarcos9@users.noreply.github.com>
2021-06-06 19:07:40 -0300

    Msg Erro e sucesso: add em settings MESSAGE_TAGS e importado biblioteca messages, criado html alertas e passado bootstrap com cod python. na Views de usuarios cadsatro e login add mensagens de sucesso e erro

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.models import User
-from django.contrib import auth
+from django.contrib import auth, messages
+from receitas.models import Receita
 
 
 def cadastro(request):
@@ -21,15 +22,19 @@ def cadastro(request):
             return redirect('cadastro')
 
         if senha != senha2:
+            messages.error(request, 'As senhas não são iguais!')
             print('senha direntes')
             return redirect('cadastro')
 
         if User.objects.filter(email=email).exists():
             print('usuario já cadastrado')
             return redirect('cadastro')
+        #atribui nome, email e senha a user
         user = User.objects.create_user(username=nome, email=email, password=senha)
+        # salva no bd
         user.save()
         print('usuaeio cadastrado com sucesso')
+        messages.success(request, 'Cadastro efetuado com sucesso!')
         return redirect('login')
     else:
         return render(request, 'usuarios/cadastro.html')
@@ -41,12 +46,13 @@ def login(request):
         senha = request.POST['senha']
 
         if email == "" or senha == "":
-            print('email e senha não podem icar em branco')
+            print('email e senha não podem ficar em branco')
             return redirect('login')
-
+        #Atribui email a variavel nome para poder fazer login... django autentica com username
         if User.objects.filter(email=email).exists():
             nome = User.objects.filter(email=email).values_list('username', flat=True).get()
             user = auth.authenticate(request, username=nome, password=senha)
+            #faz o login no bd
             if user is not None:
                 auth.login(request, user)
                 print('login realizdo')
@@ -55,8 +61,51 @@ def login(request):
 
 
 def dashboard(request):
-    return render(request, 'usuarios/dashboard.html')
+    #checa se usuario está autenticado e so vai para dashboard caso True
+    if request.user.is_authenticated:
+        #identifica o criado da receita
+        id = request.user.id
+        #armazena e lista por ordem de data as resceitas
+        receitas = Receita.objects.order_by('-data_receita').filter(pessoa=id)
+
+        #passar as informaçoes para template
+        dados = {
+            'receitas': receitas
+        }
+
+
+        return render(request, 'usuarios/dashboard.html', dados)
+    else:
+        return redirect('index')
 
 
 def logout(request):
-    pass
+    auth.logout(request)
+    return redirect('index')
+
+
+def cria_receita(request):
+    if request.method == 'POST':
+        nome_receita = request.POST['nome_receita']
+        ingredientes = request.POST['ingredientes']
+        modo_preparo = request.POST['modo_preparo']
+        tempo_preparo = request.POST['tempo_preparo']
+        rendimento = request.POST['rendimento']
+        categoria = request.POST['categoria']
+        imagem_receita = request.FILES['imagem_receita']
+
+        user = get_object_or_404(User, pk=request.user.id)
+        receita = Receita.objects.create(pessoa=user,
+                                         nome_receita=nome_receita,
+                                         ingredientes=ingredientes,
+                                         modo_preparo=modo_preparo,
+                                         tempo_preparo=tempo_preparo,
+                                         rendimento=rendimento,
+                                         categoria=categoria,
+                                         imagem_receita=imagem_receita)
+
+        receita.save()
+
+        return redirect('dashboard')
+    else:
+        return render(request, 'usuarios/cria_receita.html')
